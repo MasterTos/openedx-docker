a1d806a95b174f3b8e689606bcebdc13f2477333
MasterTos <MasterTos@yahoo.com>
2018-12-24 14:10:05 +0700

    add reg

--- configurator/templates/letsencrypt/certonly.sh
+++ configurator/templates/letsencrypt/certonly.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-certbot certonly --standalone -n --agree-tos -m admin@{{ LMS_HOST }} -d {{ LMS_HOST }} -d {{ CMS_HOST }} -d preview.{{ LMS_HOST }} 
+certbot certonly --standalone -n --agree-tos -m admin@{{ LMS_HOST }} -d {{ LMS_HOST }} -d {{ CMS_HOST }} -d preview.{{ LMS_HOST }} -d reg.{{ LMS_HOST }} 
 
 {% if ACTIVATE_NOTES %} 
 certbot certonly --standalone -n --agree-tos -m admin@{{ LMS_HOST }} -d notes.{{ LMS_HOST }}
